5a32118e7a4957ffdff9571140fb22ad65410389
fkyrz <hiroky.fukushima@icloud.com>
2024-10-26 19:22:44 +0900

    fix: get.goの実装

--- backend/app/usecase/music_sheet_usecase/get.go
+++ backend/app/usecase/music_sheet_usecase/get.go
@@ -5,10 +5,22 @@ import (
 
     "github.com/furu2revival/musicbox/app/core/request_context"
     "github.com/furu2revival/musicbox/app/domain/model"
-    "github.com/furu2revival/musicbox/app/adapter/repoimpl/music_sheet_repoimpl"
+	"github.com/furu2revival/musicbox/app/domain/repository/transaction"
 	"github.com/google/uuid"
 )
 
 func (u Usecase) Get(ctx context.Context, rctx request_context.RequestContext, id uuid.UUID) (model.MusicSheet, error) {
-	
+	var musicSheet model.MusicSheet
+	err := u.conn.BeginRwTransaction(ctx, func(ctx context.Context, tx transaction.Transaction) error {
+		var err error
+		musicSheet, err = u.musicSheetRepo.Get(ctx, tx, id)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return model.MusicSheet{}, err
+	}
+	return musicSheet, nil
 }
