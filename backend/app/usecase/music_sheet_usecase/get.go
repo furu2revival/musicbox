7979101c233760233804fd09749df5deab5817fd
fkyrz <hiroky.fukushima@icloud.com>
2024-10-26 19:25:29 +0900

    fix: Get()を修正

--- backend/app/usecase/music_sheet_usecase/get.go
+++ backend/app/usecase/music_sheet_usecase/get.go
@@ -11,7 +11,7 @@ import (
 
 func (u Usecase) Get(ctx context.Context, rctx request_context.RequestContext, id uuid.UUID) (model.MusicSheet, error) {
 	var musicSheet model.MusicSheet
-	err := u.conn.BeginRwTransaction(ctx, func(ctx context.Context, tx transaction.Transaction) error {
+	err := u.conn.BeginRoTransaction(ctx, func(ctx context.Context, tx transaction.Transaction) error {
 		var err error
 		musicSheet, err = u.musicSheetRepo.Get(ctx, tx, id)
 		if err != nil {
