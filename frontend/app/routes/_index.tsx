b5ef0d86bcbc0c667ea694dab20d7eecda839085
tatsuya-abe <tatsuya.abe@dena.com>
2024-10-26 17:06:14 +0900

    refs #21 connect-queryの設定を定義した

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,5 +1,9 @@
+import { TransportProvider } from "@bufbuild/connect-query";
+import { createConnectTransport } from "@connectrpc/connect-web";
 import type { MetaFunction } from "@remix-run/node";
 import MyButton from "../components/button";
+import { QueryClient } from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
 
 export const meta: MetaFunction = () => {
 	return [
@@ -8,10 +12,31 @@ export const meta: MetaFunction = () => {
 	];
 };
 
+const finalTransport = createConnectTransport({
+	baseUrl: "http://43.207.202.40:8000",
+	// 開発期間は、binary ではなく JSON 形式を用いる。
+	// 理由: ブラウザの開発者ツールで human-readable なリクエスト/レスポンスメッセージを確認できるため。
+	useBinaryFormat: false,
+});
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			queryKeyHashFn: (object: unknown) =>
+				JSON.stringify(object, (_, value) => {
+					// BigInt 型は JSON に変換できないので、文字列に変換する。
+					return typeof value === "bigint" ? value.toString() : value;
+				}),
+		},
+	},
+});
+
 export default function Index() {
 	return (
-		<div>
-			<MyButton />
-		</div>
+		<TransportProvider transport={finalTransport}>
+			<QueryClientProvider client={queryClient}>
+				<MyButton />
+			</QueryClientProvider>
+		</TransportProvider>
 	);
 }
