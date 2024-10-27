9c20589ceed64c9ca167abf2d7312620590419d5
cou <courange.c@gmail.com>
2024-10-27 14:09:20 +0900

    add start stop button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { queryClient } from "./queryClient.ts";
+import "./main.module.css";
 
 const finalTransport = createConnectTransport({
 	baseUrl: "https://musicbox.averak.net",
