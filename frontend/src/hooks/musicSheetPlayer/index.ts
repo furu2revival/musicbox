c6f6ffda4e69ae16176366d005c68c09a68d01db
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-27 10:11:35 +0900

    feat: add wind_sprint sound

--- frontend/src/hooks/musicSheetPlayer/index.ts
+++ frontend/src/hooks/musicSheetPlayer/index.ts
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 
-import {
-	createMusicSheetPlayer,
-	MusicSheetPlayerInit,
+import type {
 	MusicSheetPlayer,
+	MusicSheetPlayerInit,
 } from "./musicSheetPlayer";
+import { createMusicSheetPlayer } from "./musicSheetPlayer";
+import {
+	type PeriodicAudioPlayer,
+	createPeriodicAudioPlayer,
+} from "./periodicAudioPlayer";
+
+import windSprintSound from "~/assets/wind_spring.wav";
 
 export const useMusicSheetPlayer = function (init: MusicSheetPlayerInit) {
 	const [player, setPlayer] = useState<MusicSheetPlayer | null>(null);
-	const [energy, setEnergy] = useState<number>(0);
+	const [windPlayer, setWindPlayer] = useState<PeriodicAudioPlayer | null>(
+		null
+	);
+	const [oldEnergy, setOldEnergy] = useState<number>(0);
 
 	useEffect(() => {
 		(async () => {
@@ -18,25 +27,49 @@ export const useMusicSheetPlayer = function (init: MusicSheetPlayerInit) {
 	}, [...Object.values(init)]);
 
 	useEffect(() => {
+		(async () => {
+			const player = await createPeriodicAudioPlayer(windSprintSound, 0.001);
+			setWindPlayer(player);
+		})();
+	});
+
+	useEffect(() => {
 		const listener = () => {
-			setEnergy(player?.energy || 0);
+			const newEnergy = player?.energy ?? 0;
+			const maxEnergy = player?.maxEnergy;
+			if (typeof maxEnergy === "number" && oldEnergy < newEnergy) {
+				const volume = oldEnergy / maxEnergy;
+
+				if (volume < 0.5) {
+					windPlayer?.play(50, 10);
+				} else if (volume < 0.75) {
+					windPlayer?.play(100, 10);
+				} else if (volume < 0.9) {
+					windPlayer?.play(200, 5);
+				} else {
+					windPlayer?.play(500, 3);
+				}
+			}
+
+			setOldEnergy(newEnergy);
 		};
 		player?.addEventListener("energychange", listener);
 
 		return () => {
 			player?.removeEventListener("energychange", listener);
 		};
-	}, [player]);
+	}, [player, windPlayer]);
 
 	const play = () => player?.play();
 	const stop = () => player?.stop();
 	const setPlayerEnergy = (energy: number) => player?.setEnergy(energy);
 
 	return {
-		ready: player !== null,
+		ready: player !== null && windPlayer !== null,
 		play,
 		stop,
-		energy,
+		energy: player?.energy,
 		setEnergy: setPlayerEnergy,
+		maxEnergy: player?.maxEnergy,
 	};
 };
