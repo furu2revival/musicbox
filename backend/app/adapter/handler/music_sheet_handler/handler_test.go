0366da16beaad73aab3cdfd9663b0524ccfff9e4
tatsuya-abe <tatsuya.abe@dena.com>
2024-10-27 10:37:35 +0900

    refs #12 MusicSheetService/GetV1の結合テストを作成した

--- backend/app/adapter/handler/music_sheet_handler/handler_test.go
+++ backend/app/adapter/handler/music_sheet_handler/handler_test.go
@@ -8,6 +8,7 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/furu2revival/musicbox/app/adapter/dao"
+	"github.com/furu2revival/musicbox/app/domain/model"
 	"github.com/furu2revival/musicbox/app/registry"
 	"github.com/furu2revival/musicbox/protobuf/api"
 	"github.com/furu2revival/musicbox/protobuf/api/api_errors"
@@ -19,7 +20,107 @@ import (
 	"github.com/furu2revival/musicbox/testutils/testconnect"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/volatiletech/sqlboiler/v4/types"
 )
+
+func Test_handler_GetV1(t *testing.T) {
+	mux, err := registry.InitializeAPIServerMux(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	type given struct {
+		seeds []fixture.Seed
+	}
+	type when struct {
+		req  *api.MusicSheetServiceGetV1Request
+		opts []testconnect.Option
+	}
+	type then = func(*testing.T, *connect.Response[api.MusicSheetServiceGetV1Response], error)
+	tests := []bdd.Testcase[given, when, then]{
+		{
+			Name: "楽譜が存在する状態で",
+			Given: given{
+				seeds: []fixture.Seed{
+					&dao.MusicSheet{
+						MusicSheetID: faker.UUIDv5("ms1").String(),
+						Title:        "ms1",
+					},
+					&dao.Note{
+						Index:        0,
+						MusicSheetID: faker.UUIDv5("ms1").String(),
+						Pitches: types.Int64Array{
+							int64(model.PitchC3),
+						},
+					},
+					&dao.Note{
+						Index:        1,
+						MusicSheetID: faker.UUIDv5("ms1").String(),
+						Pitches: types.Int64Array{
+							int64(model.PitchC3),
+							int64(model.PitchD3),
+						},
+					},
+				},
+			},
+			Behaviors: []bdd.Behavior[when, then]{
+				{
+					Name: "ID が存在する => 取得できる",
+					When: when{
+						req: &api.MusicSheetServiceGetV1Request{
+							MusicSheetId: faker.UUIDv5("ms1").String(),
+						},
+					},
+					Then: func(t *testing.T, got *connect.Response[api.MusicSheetServiceGetV1Response], err error) {
+						require.NoError(t, err)
+
+						want := &api.MusicSheetServiceGetV1Response{
+							MusicSheet: &api.MusicSheet{
+								MusicSheetId: faker.UUIDv5("ms1").String(),
+								Title:        "ms1",
+								Notes: []*api.Note{
+									{
+										Pitches: []api.Pitch{api.Pitch_PITCH_C3},
+									},
+									{
+										Pitches: []api.Pitch{api.Pitch_PITCH_C3, api.Pitch_PITCH_D3},
+									},
+								},
+							},
+						}
+						assert.EqualExportedValues(t, want, got.Msg)
+					},
+				},
+				{
+					Name: "ID が存在しない => エラー",
+					When: when{
+						req: &api.MusicSheetServiceGetV1Request{
+							MusicSheetId: faker.UUIDv5("not-exists").String(),
+						},
+					},
+					Then: func(t *testing.T, got *connect.Response[api.MusicSheetServiceGetV1Response], err error) {
+						testconnect.AssertErrorCode(t, api_errors.ErrorCode_METHOD_RESOURCE_NOT_FOUND, err)
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt.Run(t, func(t *testing.T, given given, when when, then then) {
+			fixture.SetupSeeds(t, context.Background(), given.seeds...)
+			defer testutils.Teardown(t)
+
+			got, err := testconnect.MethodInvoke(
+				apiconnect.NewMusicSheetServiceClient(http.DefaultClient, server.URL).GetV1,
+				when.req,
+				when.opts...,
+			)
+			then(t, got, err)
+		})
+	}
+}
 
 func Test_handler_CreateV1(t *testing.T) {
 	mux, err := registry.InitializeAPIServerMux(context.Background())
