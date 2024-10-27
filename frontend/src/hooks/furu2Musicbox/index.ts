65487a92dd9da7b814333f3f803415d57bf7b3ae
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-27 12:14:41 +0900

    style: apply formatter

--- frontend/src/hooks/furu2Musicbox/index.ts
+++ frontend/src/hooks/furu2Musicbox/index.ts
@@ -13,83 +13,87 @@ import { createShakeDetector, ShakeDetector } from "~/features/shakeDetecters";
 import windSprintSound from "~/assets/wind_spring.wav";
 
 type Furu2MusicPlayerInit = {
-  playerInit: MusicSheetPlayerInit,
-  shakeDetectorInit: ShakeDetectorInit,
-}
+	playerInit: MusicSheetPlayerInit;
+	shakeDetectorInit: ShakeDetectorInit;
+};
 
-export const useMusicBox = function(init: Furu2MusicPlayerInit) {
-  const [player, setPlayer] = useState<MusicSheetPlayer | null>(null);
-	const [windPlayer, setWindPlayer] = useState<PeriodicAudioPlayer | null>(null);
-  const [shakeDetector, setShakeDetector] = useState<ShakeDetector | null>(null);
-  const [energy, setEnergy] = useState<number>(0);
-  const [maxEnergy, setMaxEnergy] = useState<number>(0);
+export const useMusicBox = function (init: Furu2MusicPlayerInit) {
+	const [player, setPlayer] = useState<MusicSheetPlayer | null>(null);
+	const [windPlayer, setWindPlayer] = useState<PeriodicAudioPlayer | null>(
+		null
+	);
+	const [shakeDetector, setShakeDetector] = useState<ShakeDetector | null>(
+		null
+	);
+	const [energy, setEnergy] = useState<number>(0);
+	const [maxEnergy, setMaxEnergy] = useState<number>(0);
 
-  const loadPlayer = async function() {
-    const player = await createMusicSheetPlayer(init.playerInit);
-    setPlayer(player);
-  }
-  const loadWindPlayer = async function() {
-    const player = await createPeriodicAudioPlayer(windSprintSound, 0.001);
-    setWindPlayer(player);
-  }
-  const loadShakeDetector = async function() {
-    const detector = await createShakeDetector(init.shakeDetectorInit);
-    setShakeDetector(detector);
-  }
-  const load = function() {
-    loadPlayer();
-    loadWindPlayer();
-    loadShakeDetector();
-  }
+	const loadPlayer = async function () {
+		const player = await createMusicSheetPlayer(init.playerInit);
+		setPlayer(player);
+	};
+	const loadWindPlayer = async function () {
+		const player = await createPeriodicAudioPlayer(windSprintSound, 0.001);
+		setWindPlayer(player);
+	};
+	const loadShakeDetector = async function () {
+		const detector = await createShakeDetector(init.shakeDetectorInit);
+		setShakeDetector(detector);
+	};
+	const load = function () {
+		loadPlayer();
+		loadWindPlayer();
+		loadShakeDetector();
+	};
 
-  useEffect(() => {
-    const handleShake = () => {
-      if(!player) return;
+	useEffect(() => {
+		const handleShake = () => {
+			if (!player) return;
 
-      const energy = player.energy;
+			const energy = player.energy;
 			const maxEnergy = player.maxEnergy;
 
-      const volume = energy / maxEnergy;
+			const volume = energy / maxEnergy;
 
-      if (volume < 0.5) {
-        windPlayer?.play(50, 10);
-      } else if (volume < 0.75) {
-        windPlayer?.play(100, 10);
-      } else if (volume < 0.9) {
-        windPlayer?.play(200, 5);
-      } else {
-        windPlayer?.play(500, 3);
-      }
+			if (volume < 0.5) {
+				windPlayer?.play(50, 10);
+			} else if (volume < 0.75) {
+				windPlayer?.play(100, 10);
+			} else if (volume < 0.9) {
+				windPlayer?.play(200, 5);
+			} else {
+				windPlayer?.play(500, 3);
+			}
 
-      player?.setEnergy(energy + 1);
-    };
+			player?.setEnergy(energy + 1);
+		};
 
-    shakeDetector?.addEventListener('shake', handleShake);
+		shakeDetector?.addEventListener("shake", handleShake);
 
-    return () => {
-      shakeDetector?.removeEventListener('shake', handleShake);
-    }
-  }, [player, shakeDetector]);
-  
-  useEffect(() => {
-    if(!player) return;
+		return () => {
+			shakeDetector?.removeEventListener("shake", handleShake);
+		};
+	}, [player, shakeDetector]);
 
-    const handleEnergyChange = () => {
-      setEnergy(player.energy);
-      setMaxEnergy(player.maxEnergy);
-    };
-    player.addEventListener('energychange', handleEnergyChange);
-    return () => {
-      player.removeEventListener('energychange', handleEnergyChange);
-    }
-  }, [player]);
+	useEffect(() => {
+		if (!player) return;
 
-  return {
-    ready: player !== null && windPlayer !== null && shakeDetector !== null,
-    load,
-    play: () => player?.play(),
-    stop: () => player?.stop(),
-    energy,
-    maxEnergy,
-  }
-}
+		const handleEnergyChange = () => {
+			setEnergy(player.energy);
+			setMaxEnergy(player.maxEnergy);
+		};
+		player.addEventListener("energychange", handleEnergyChange);
+		return () => {
+			player.removeEventListener("energychange", handleEnergyChange);
+		};
+	}, [player]);
+
+	return {
+		ready: player !== null && windPlayer !== null && shakeDetector !== null,
+		load,
+		play: () => player?.play(),
+		stop: () => player?.stop(),
+		energy,
+		maxEnergy,
+	};
+};
